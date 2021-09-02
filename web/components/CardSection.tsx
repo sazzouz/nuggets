57d825a73b0a19eb6a6f3bc785f6dbf3bc86cf1a
Samir Azzouz <s.azzouz97@gmail.com>
2021-09-02 20:12:02 +0100

    fixing vercel build bug due to empty component filed breaking compilation steps

---
+++ web/components/CardSection.tsx
@@ -0,0 +1,11 @@
+export interface CardSectionProps {}
+
+const CardSection: React.SFC<CardSectionProps> = ({ props }) => {
+    return (
+        <>
+            <div>Card section</div>
+        </>
+    );
+};
+
+export default CardSection;
